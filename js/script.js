cd02e0e540113da99ada5a2abccc6a09700a5372
Igor Tozetti <igorct77@gmail.com>
2022-10-27 01:50:40 -0300

    accordion list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import SmoothScroll from "./smoothscroll.js";
+import Accordion from "./accordion.js";
 
 const smoothScroll = new SmoothScroll('[data-anime="smooth"] a[href^="#"]');
 smoothScroll.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();

4ebca8e4258af465d24e3dd40a7fbf1aac05e56e
Igor Tozetti <igorct77@gmail.com>
2022-10-27 22:25:36 -0300

    menu-mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import SmoothScroll from "./smoothscroll.js";
 import Accordion from "./accordion.js";
 import Modal from "./modal.js";
+import MenuMobile from "./menu-mobile.js";
+
 const smoothScroll = new SmoothScroll('[data-anime="smooth"] a[href^="#"]');
 smoothScroll.init();
 
@@ -14,3 +16,9 @@ const modal = new Modal(
 );
 
 modal.init();
+
+const menuMobile = new MenuMobile(
+  '[data-menu="hamburguer"]',
+  '[data-menu="list"]'
+);
+menuMobile.init();
